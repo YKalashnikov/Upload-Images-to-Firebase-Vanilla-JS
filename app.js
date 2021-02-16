52862d606d80935bf1dac2ba7bfab30433ebd979
YKalashnikov <uk2709@gmail.com>
2021-02-15 23:50:36 -0800

    Added Firebase

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
-import {upload} from './upload.js';
+import {
+    upload
+} from './upload.js';
+
+import dotenv from 'dotenv';
+
+
+import firebase from 'firebase/app'
+import 'firebase/storage';
+
+dotenv.config()
+
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage()
+
 
 upload('#file', {
     multiple: true,
     accept: ['.png', '.gif', '.bmp', 'jpeg', 'jpg'],
-    onUpload(files) {
-        console.log(files)
+    onUpload(files, blocks) {
+        files.forEach((file, index) => {
+            const ref = storage.ref(`images/${file.name}`)
+            const task = ref.put(file)
+            task.on('state_changes', snapshot => {
+                const percentage = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0) + '%';
+                const block = blocks[index].querySelector('.preview-info-progress')
+                block.textContent = percentage
+                block.style.width = percentage
+            }, error => {
+                console.log(error)
+            }, () => {
+                task.snapshot.ref.getDownloadURL().then(url => {
+                    console.log('URL', url)
+                })
+            })
+        })
     }
 })
